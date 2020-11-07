f4a61fd1777e4c5f8c8316c86f58a4ff03267611
Jeferson Borges <jefersonmartinsborges@hotmail.com>
2020-11-06 21:01:49 -0300

    Adiciona HealthCheck

---
+++ Pets.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Pets.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Check() => Ok(new { message = "Server is running..." });
+
+    }
+}
